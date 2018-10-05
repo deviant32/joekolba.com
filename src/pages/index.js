cecc527b58681efb39391eddf2edf8812ebb6f97
Joe Kolba <joekolba09@gmail.com>
2018-10-05 11:59:20 -0400

    added netlify tag

--- src/pages/index.js
+++ src/pages/index.js
@@ -164,7 +164,7 @@ export default class IndexPage extends React.Component {
         </section>
         <Modal open={open} onClose={this.onCloseModal} center>
           <h3>Contact Me</h3>
-          <form id="contact-form" name="contact" action="" data-netlify="true">
+          <form id="contact-form" name="contact" action="" data-netlify="true" netlify>
             <hr />
             <div class="controls">
               <div class="row">
