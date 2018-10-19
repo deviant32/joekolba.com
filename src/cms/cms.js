25d0dcc7463e9edcbabcb4d1a7ac42857864a945
Joe Kolba <joekolba09@gmail.com>
2018-10-19 10:02:10 -0400

    added resume

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,11 +1,13 @@
 import CMS from 'netlify-cms'
 
 import AboutPagePreview from './preview-templates/AboutPagePreview'
+import ResumePagePreview from './preview-templates/ResumePagePreview'
 import BlogPostPreview from './preview-templates/BlogPostPreview'
 import NewsPostPreview from './preview-templates/NewsPostPreview'
 import ProductPagePreview from './preview-templates/ProductPagePreview'
 
 CMS.registerPreviewTemplate('about', AboutPagePreview)
+CMS.registerPreviewTemplate('resume', ResumePagePreview)
 CMS.registerPreviewTemplate('products', ProductPagePreview)
 CMS.registerPreviewTemplate('blog', BlogPostPreview)
 CMS.registerPreviewTemplate('news', BlogPostPreview)
