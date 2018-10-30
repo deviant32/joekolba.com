9924373819dd7aea2574ab77d735c830df3c05ae
Joe Kolba <joekolba09@gmail.com>
2018-10-30 13:44:06 -0400

    fixed resume page

--- src/templates/resume-page.js
+++ src/templates/resume-page.js
@@ -1,12 +1,8 @@
 import React from "react";
-import PropTypes from "prop-types";
 import { graphql } from "gatsby";
 import Layout from "../components/Layout";
-import Content, { HTMLContent } from "../components/Content";
 
-export const ResumePageTemplate = ({ title, content, contentComponent }) => {
-  const PageContent = contentComponent || Content;
-
+export const ResumePageTemplate = ({ title }) => {
   return (
     <section className="section section--gradient">
       <div className="container">
@@ -24,42 +20,19 @@ export const ResumePageTemplate = ({ title, content, contentComponent }) => {
   );
 };
 
-<<<<<<< HEAD
-ResumePageTemplate.propTypes = {
-  title: PropTypes.string.isRequired,
-  content: PropTypes.string,
-  contentComponent: PropTypes.func
-};
-
-const ResumePage = ({ data }) => {
-  const { markdownRemark: post } = data;
-=======
 const ResumePage = ({ data }) => {
   const page = data.page;
->>>>>>> e9fdc42fdfa850d67fc5704f8323d62c2f364a5d
 
   return (
     <Layout>
-      <ResumePageTemplate
-        title={page.frontmatter.title}
-      />
+      <ResumePageTemplate title={page.frontmatter.title} />
     </Layout>
   );
 };
 
-<<<<<<< HEAD
-ResumePage.propTypes = {
-  data: PropTypes.object.isRequired
-};
-
 export default ResumePage;
-=======
-export default ResumePage
->>>>>>> e9fdc42fdfa850d67fc5704f8323d62c2f364a5d
-
 export const resumePageQuery = graphql`
   query ResumePage($id: String!) {
-
     page: markdownRemark(id: { eq: $id }) {
       html
       frontmatter {
